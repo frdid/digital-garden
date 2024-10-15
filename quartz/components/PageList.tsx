6453e0f5fe9f5198580a4ffddd1a6b2b615dae1e
Ferdi Dirgantara <ferdifordev@gmail.com>
2024-10-15 13:20:43 +0800

    Quartz sync: Oct 15, 2024, 1:20 PM

--- quartz/components/PageList.tsx
+++ quartz/components/PageList.tsx
@@ -50,7 +50,7 @@ type Props = {
 } & QuartzComponentProps
 
 export const PageList: QuartzComponent = ({ cfg, fileData, allFiles, limit, sort }: Props) => {
-  const sorter = sort ?? byZettelkastenID(cfg)
+  const sorter = sort ?? byAlphabetical(cfg)
   let list = allFiles.sort(sorter)
   if (limit) {
     list = list.slice(0, limit)
@@ -61,7 +61,6 @@ export const PageList: QuartzComponent = ({ cfg, fileData, allFiles, limit, sort
       {list.map((page) => {
         const title = page.frontmatter?.title
         const tags = page.frontmatter?.tags ?? []
-        const relaltivePath = page?.relativePath
         
         return (
           <li class="section-li">
